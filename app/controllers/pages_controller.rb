5473db9fd1ad1b1d66e85c3f7da2129d8c70ac8a
Victor Otieno <victorotieno598@gmail.com>
2022-01-13 21:23:20 +0300

    Fix landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,3 @@
-# class PagesController < ApplicationController
-# end
-
 class PagesController < ActionController::Base
   def index
     redirect_to '/api-docs'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
   # api documentation
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
