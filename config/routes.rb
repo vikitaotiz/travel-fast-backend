3f7f7a2da262c24e5e6369919eff94d91da97fcf
Victor Otieno <victorotieno598@gmail.com>
2022-01-13 06:40:06 +0300

    Add swagger config and api documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # api documentation
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
   namespace :api do
     namespace :v1 do
       # authentication routes
