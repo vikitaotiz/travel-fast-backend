94b0c4985a0a9c0b55166056ae72f3f7973276fb
vikitaotiz <victorotieno598@gmail.com>
2022-01-20 20:12:45 +0300

    Add car to my reservations end point

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,7 +2,20 @@ class Api::V1::ReservationsController < ApplicationController
   def index
     # show reservations that belongs to a user
     if params[:user_id]
-      @reservations = Reservation.where(user_id: params[:user_id])
+      @reservations = []
+      Reservation.where(user_id: params[:user_id]).each do |rs|
+        data = {}
+        data['id'] = rs.id
+        data['car_id'] = rs.car_id
+        data['car_name'] = Car.find(rs.car_id).name if rs.car_id
+        data['user_id'] = rs.user_id
+        data['city'] = rs.city
+        data['start_date'] = rs.start_date
+        data['end_date'] = rs.end_date
+        data['created_at'] = rs.created_at
+
+        @reservations << data
+      end
 
       render json: { data: @reservations }, status: :created
     else
